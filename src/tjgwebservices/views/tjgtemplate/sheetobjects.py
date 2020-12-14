429bf0443161b0a6654599bf25a93c64fb33adb4
tjgwebservices <info@tjgwebservices.com>
2020-12-13 20:08:08 -0600

    Create sheetobjects.py

---
+++ src/tjgwebservices/views/tjgtemplate/sheetobjects.py
@@ -0,0 +1,12 @@
+class TemplateSheets():
+    templatetext = ""
+    mainstylesheet = ""
+    javascriptsheet = ""
+    paths = ["/", "/home",
+                "/index", "/error"]
+    csspaths = ["/static/css/home.css"]
+    jspaths = ["/static/js/home.js"]
+    socketpaths = ["/socket","/topic","/message"]
+    
+    def __init__(self):
+        pass
