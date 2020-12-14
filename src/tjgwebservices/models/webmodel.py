fee541e743be2f9f0099bd0b310c66ed6e057b17
tjgwebservices <info@tjgwebservices.com>
2020-12-13 20:00:25 -0600

    Create webmodel.py

---
+++ src/tjgwebservices/models/webmodel.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+class WebDoc(object):
+	def __init__(self, doc):
+		self.doc = doc
+
+	def retrieveValue(self,value):
+		return self.doc.getElementsByTagName(value)[0].childNodes[0].nodeValue.strip()
+
+	def retrieveMainMenu(self):
+		i = []
+		items=self.doc.getElementsByTagName('mainmenu')[0].getElementsByTagName('item')
+		for item in items:
+			ei = []
+			id = item.getAttribute('id')
+			name = item.getElementsByTagName('name')[0].childNodes[0].nodeValue.strip()
+			link = item.getElementsByTagName('link')[0].childNodes[0].nodeValue.strip()
+			submenu = item.getElementsByTagName('submenu')[0].childNodes[0].nodeValue.strip()
+			ei.append([id, name, link, submenu])
+			i.append(ei)
+		return i
+
+	def retrieveSubMenu(self):
+		i = []
+		items=self.doc.getElementsByTagName('mainsubmenu')[0].getElementsByTagName('item')
+		for item in items:
+			ei = []
+			id = item.getAttribute('id')
+			name = item.getElementsByTagName('name')[0].childNodes[0].nodeValue.strip()
+			link = item.getElementsByTagName('link')[0].childNodes[0].nodeValue.strip()
+			parentmenu = item.getElementsByTagName('parentmenu')[0].childNodes[0].nodeValue.strip()
+			submenu = item.getElementsByTagName('submenu')[0].childNodes[0].nodeValue.strip()
+			ei.append([id, name, link, parentmenu, submenu])
+			i.append(ei)
+		return i
+
+	def retrieveSubSubMenu(self):
+		i = []
+		items=self.doc.getElementsByTagName('subsubmenu')[0].getElementsByTagName('item')
+		for item in items:
+			ei = []
+			id = item.getAttribute('id')
+			name = item.getElementsByTagName('name')[0].childNodes[0].nodeValue.strip()
+			link = item.getElementsByTagName('link')[0].childNodes[0].nodeValue.strip()
+			parentmenu = item.getElementsByTagName('parentmenu')[0].childNodes[0].nodeValue.strip()
+			ei.append([id, name, link, parentmenu])
+			i.append(ei)
+		return i
+
